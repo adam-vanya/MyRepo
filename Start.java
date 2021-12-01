260a94fa0fd424bd1e430dc2f905480431e76716
Adam Vanya <adam.vanya@frontendart.com>
2021-12-01 16:04:58 +0100

    first commit

---
+++ Start.java
@@ -0,0 +1,27 @@
+
+public class Start{
+
+
+	public boolean isEven(int input){
+		if (input%2 == 0) return true;
+		else return false;
+	}
+
+	public boolean isTenaryEven(int input){
+		return input%2==0 ? true : false;
+	}
+	
+
+	public static void main(String args[]){
+
+		var instance = new Start();
+		
+		System.out.println(instance.isEven(10));
+		System.out.println(instance.isEven(13));
+
+		System.out.println(instance.isTenaryEven(10));
+		System.out.println(instance.isTenaryEven(13));
+
+	}
+
+}
